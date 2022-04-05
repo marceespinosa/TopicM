bc5b00d27181044dc7b28ef236053a14b25ac2f2
marceespinosa <78478868+marceespinosa@users.noreply.github.com>
2022-04-05 21:06:24 +0100

    Add files via upload

--- FileProcessor.java
+++ FileProcessor.java
@@ -2,49 +2,128 @@ package com_file.OO;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.Scanner;
 
 
 public class FileProcessor 
 {
-	
 	//method to count the words, uses a file name a map
-	public static void countwords (String filename, Map<String, Integer> words) 
+	public static void countwords (String filename, Map<String, Integer> words) throws IOException 
 	{
+		ArrayList<String> stopwords = new ArrayList<String>();
 		Scanner file;
+		String wordintext;
+	
+		
 		try {
-			file = new Scanner (new File(filename));
+				file = new Scanner (new File(filename));
+				LineNumberReader w = new LineNumberReader (new FileReader ("StopWords"));
+				String line;
+				
+			while((line = w.readLine()) !=null )
+			{
+				for (String textword : line.toLowerCase().split(""))
+				{
+					stoplist.add(line);
+				}
+				
+			}
+			
+				
 			while(file.hasNext()) // if the scanner has a next like or next word it will go thorough a loop and count the word 
 			{
+				
 				String word = file.next().toLowerCase();
 				Integer count = words.get(word);
+				
 				if(count!=null)
 				{
 					count++;
 					
-				}
+				}//end if
 				
 				else 
 				{
 					count = 1;
 					
 				
+				}//end else
+				if (stopwords.contains(word))
+				{
+					count = null;
+					word = null;
 				}
+				
 				words.put(word, count);
-			}
+			}//end while
+			
 			file.close();
-		} catch (FileNotFoundException e) 
+			
+		
+		}//end try
+		
+		catch (FileNotFoundException e) 
 		{
 			e.printStackTrace();
-		}  
+		}//end catch
 		
-	}
+	}//end count words
+	
+	
+//	public static String [] readStopWords(String stopFileName)
+//	{
+//		String[] stopword = null;
+//		
+//			try 
+//			{
+//				Scanner stopwords = new Scanner (new File(stopFileName));
+//				stopwords.close();
+//			} 
+//			
+//			catch (FileNotFoundException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//			
+//		return stopword;
+//		
+//		
+//	}
+//	public static void removeStopWords(String fileName, String[] stopwords)
+//	{
+//		String stop;
+//		
+//		try 
+//		{
+//			Scanner file = new Scanner(new File (fileName));
+//			
+//			while (file.hasNext())
+//			{
+//				stop = file.next();
+//				
+//				if(isStopWord(word, ))
+//			}
+//			
+//		} //end try
+//		
+//		catch (FileNotFoundException e) 
+//		
+//		{
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}//end catch
+//		
+//	}//end removestopwords
 
 
 		
 		
-}
+}//end File Processors
 
 	
 
